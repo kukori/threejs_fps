f91da7227221d2209aa2879294cc9a4fc0396c9b
kukori <attila.varjas@gmail.com>
2022-05-21 15:02:38 +0200

    Scene outsorced to its own class 2

---
+++ src/FPSScene.ts
@@ -0,0 +1,27 @@
+import * as THREE from 'three'
+
+export default class FPSScene extends THREE.Scene
+{
+    initialze()
+    {
+        const geometry = new THREE.BoxGeometry()
+        const material = new THREE.MeshPhongMaterial({ color: 0xFFAD00 })
+
+        const cube = new THREE.Mesh(geometry, material)
+        cube.position.z = -5
+        cube.position.y = -1
+
+        this.add(cube)
+
+        const light = new THREE.DirectionalLight(0xFFFFFF, 1)
+        light.position.set(0, 4, 2)
+
+        this.add(light)
+    }
+
+    update()
+    {
+
+    }
+
+}
