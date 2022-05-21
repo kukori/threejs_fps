16e3db07ef8f2af1a4f8f42fe14106af623e0559
kukori <attila.varjas@gmail.com>
2022-05-21 15:02:07 +0200

    Scene outsorced to its own class

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import FPSScene from './FPSScene'
 
 const sizes = {
     width: window.innerWidth,
@@ -27,21 +28,14 @@ renderer.setSize(sizes.width, sizes.height)
 
 const mainCamera = new THREE.PerspectiveCamera(60, sizes.width/sizes.height, 0.1, 100)
 
-const scene = new THREE.Scene()
+const scene = new FPSScene()
+scene.initialze()
 
-const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshPhongMaterial({ color: 0xFFAD00 })
+function tick() 
+{
+  scene.update()
+  renderer.render(scene, mainCamera)
+  requestAnimationFrame(tick)
+}
 
-const cube = new THREE.Mesh(geometry, material)
-cube.position.z = -5
-cube.position.y = 1
-
-scene.add(cube)
-
-const light = new THREE.DirectionalLight(0xFFFFFF, 1)
-light.position.set(0, 4, 2)
-
-scene.add(light)
-
-
-renderer.render(scene, mainCamera)
+tick()
